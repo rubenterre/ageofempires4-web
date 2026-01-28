8375bc77ea0466f0deec28adc1f0b355e40cc5f1
Rubén Terré <72130385+rubenterre@users.noreply.github.com>
2026-01-28 13:35:07 +0100

    Update: config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    base: '/ageofempires4-web'
+    output: 'static'
 });
